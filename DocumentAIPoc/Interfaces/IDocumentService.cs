f4e3c0113650d7a927cde0e428a52aa5c9512e56
ROOT_MILLIMAN\Javier.Cordova <javier.cordova@milliman.com>
2024-05-06 11:58:35 -0700

    document interfaces

--- DocumentAIPoc/Interfaces/IDocumentService.cs
+++ DocumentAIPoc/Interfaces/IDocumentService.cs
@@ -5,7 +5,18 @@ namespace DocumentAIPoc.Interfaces
 {
     public interface IDocumentService
     {
+        /// <summary>
+        /// Analyze a document using the Document Analysis service.
+        /// </summary>
+        /// <param name="file">The file stream to be analyzed.</param>
+        /// <returns>The result of the analysis.</returns>
         Task<AnalyzeResult?> AnalyzeDocument(Stream file);
+
+        /// <summary>
+        /// Generate an Excel spreadsheet from a list of tables.
+        /// </summary>
+        /// <param name="tables">Tables containing rows and cols to generate the spreadsheet.</param>
+        /// <returns>The excel package with the sheets and cells generated.</returns>
         ExcelPackage GenerateTablesSpreadsheet(IReadOnlyList<DocumentTable> tables);
     }
 }
--- DocumentAIPoc/Interfaces/IFileManager.cs
+++ DocumentAIPoc/Interfaces/IFileManager.cs
@@ -4,8 +4,20 @@ namespace DocumentAIPoc.Interfaces
 {
     public interface IFileManager
     {
+        /// <summary>
+        /// Get a file from the file share.
+        /// </summary>
+        /// <param name="filename">The filename to get.</param>
+        /// <returns>The file stream.</returns>
         Stream GetFile(string filename);
+
+        /// <summary>
+        /// Get a file client from the file share.
+        /// </summary>
+        /// <param name="filename">The filename of the file to use.</param>
+        /// <returns>The file client.</returns>
         ShareFileClient GetFileClient(string filename);
+
         //string GetCleanFilename(string filename);
     }
 }
